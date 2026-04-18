df39a934505bc1551381702ee0ee743cfe4e858d
Antonio Calderon <calderon.antoniop410@gmail.com>
2026-04-18 11:45:54 -0700

    comment deletion

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'movie-explorer', // 👈 Make sure this matches your repo name exactly
+  base: 'movie-explorer',
 })
